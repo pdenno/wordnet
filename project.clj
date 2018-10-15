6df42fff085ad7e4b0bb3506cc6c73ed99f8e38d
Duncan McGreggor <oubiwann@gmail.com>
2018-10-14 22:24:30 -0500

    Added lein alias for installing local JWI.

--- project.clj
+++ project.clj
@@ -1,13 +1,16 @@
 (defproject clojusc/wordnet "0.4.0-SNAPSHOT"
   :description "A WordNet/JWI wrapper library"
-  :url "https://github.com/delver/clj-wordnet"
-  :license {:name "Creative Commons 3.0"
-            :url "http://creativecommons.org/licenses/by/3.0/legalcode"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [edu.mit/jwi "2.3.3"]]
-  :source-path "src"
-  :min-lein-version "2.3.4"
+  :url "https://github.com/clojusc/wordnet"
+  :license {
+    :name "Creative Commons 3.0"
+    :url "http://creativecommons.org/licenses/by/3.0/legalcode"}
+  :dependencies [
+    [org.clojure/clojure "1.6.0"]
+    [edu.mit/jwi "2.4.0"]]
   :global-vars { *warn-on-reflection* true}
-  :repositories [["delver" {:url "http://repo.delver.io/releases"
-                            :snapshots false
-                            :update :always}]])
+  :profiles {
+    :dev {
+      :plugins [
+        [lein-shell "0.5.0"]]}}
+  :aliases {
+    "install-jwi" ["shell" "resources/scripts/install-jwi.sh"]})
