ef377f4c48770be2fd75e0aa8df35ce3af15e6d1
Duncan McGreggor <oubiwann@gmail.com>
2018-10-14 23:48:33 -0500

    Cut 0.4.0 release.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clojusc/wordnet "0.4.0-SNAPSHOT"
+(defproject clojusc/wordnet "0.4.0"
   :description "A WordNet/JWI wrapper library"
   :url "https://github.com/clojusc/wordnet"
   :license {
     :name "Creative Commons 3.0"
     :url "http://creativecommons.org/licenses/by/3.0/legalcode"}
   :dependencies [
-    [clojusc/jwi "2.4.0-SNAPSHOT"]
+    [clojusc/jwi "2.4.0"]
     [org.clojure/clojure "1.9.0"]]
   :global-vars { *warn-on-reflection* true}
   :profiles {
